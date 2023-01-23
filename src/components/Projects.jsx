efcdbb8c47425427fc253620db4f072f5977b231
Braian Molina <braian_gabriel_1993@hotmail.com>
2023-01-23 09:18:34 -0300

    se modifican estilos de card mobile

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,15 @@
 import { Card } from "../components";
 
 export const Projects = () => {
+  // ----------------------
+  // medidas <Card/> mobile
+  // ----------------------
+  /*
+    h-[18.625rem] = (h-36 * 2) + gap-2.5
+    w-[calc(100%-2.5rem)] = gap-10 (20px padding-left/right)
+    max-w-[30.625rem] = (w-60 * 2) + gap-2.5
+  */
+
   return (
     <>
       {/* Tittle */}
@@ -10,10 +19,14 @@ export const Projects = () => {
       </section>
 
       {/* Cards */}
-      {/* <section className="flex flex-wrap justify-center gap-2.5"> */}
-      <section className="mx-auto flex h-[298px] w-[280px] flex-col flex-wrap gap-2.5 overflow-hidden overflow-x-scroll">
+      <section
+        className={`mx-auto flex h-[18.625rem] w-[calc(100%-2.5rem)] max-w-[30.625rem] flex-col flex-wrap gap-2.5 overflow-hidden overflow-x-scroll`}
+      >
         <Card tittle="Buzón de correo" cardImage={1} vertical={false} />
         <Card tittle="Viajes" cardImage={2} vertical={false} />
+        <Card tittle="To do list" cardImage={3} vertical={true} />
+        <Card tittle="Datos inmobiliarios" cardImage={4} vertical={true} />
+
         <Card tittle="To do list" cardImage={3} vertical={true} />
         <Card tittle="Datos inmobiliarios" cardImage={4} vertical={true} />
       </section>
