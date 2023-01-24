ac909b110542cc9d7e81dc26b1d466479a154811
Braian Molina <braian_gabriel_1993@hotmail.com>
2023-01-24 09:52:43 -0300

    se modifican estilos de cards proyectos

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,15 +1,6 @@
 import { Card } from "../components";
 
 export const Projects = () => {
-  // ----------------------
-  // medidas <Card/> mobile
-  // ----------------------
-  /*
-    h-[18.625rem] = (h-36 * 2) + gap-2.5
-    w-[calc(100%-2.5rem)] = gap-10 (20px padding-left/right)
-    max-w-[30.625rem] = (w-60 * 2) + gap-2.5
-  */
-
   return (
     <>
       {/* Tittle */}
@@ -19,16 +10,18 @@ export const Projects = () => {
       </section>
 
       {/* Cards */}
-      <section
-        className={`mx-auto flex h-[18.625rem] w-[calc(100%-2.5rem)] max-w-[30.625rem] flex-col flex-wrap gap-2.5 overflow-hidden overflow-x-scroll`}
-      >
-        <Card tittle="Buzón de correo" cardImage={1} vertical={false} />
-        <Card tittle="Viajes" cardImage={2} vertical={false} />
-        <Card tittle="To do list" cardImage={3} vertical={true} />
-        <Card tittle="Datos inmobiliarios" cardImage={4} vertical={true} />
+      <section className={`flex w-full justify-center px-5`}>
+        <section
+          className={`flex h-80 w-full max-w-lg flex-col flex-wrap items-center gap-2.5 overflow-hidden overflow-x-scroll`}
+        >
+          <Card tittle="Buzón de correo" cardImage={1} vertical={false} />
+          <Card tittle="Viajes" cardImage={2} vertical={false} />
+          <Card tittle="To do list" cardImage={3} vertical={true} />
+          <Card tittle="Datos inmobiliarios" cardImage={4} vertical={true} />
 
-        <Card tittle="To do list" cardImage={3} vertical={true} />
-        <Card tittle="Datos inmobiliarios" cardImage={4} vertical={true} />
+          <Card tittle="To do list" cardImage={3} vertical={true} />
+          <Card tittle="Datos inmobiliarios" cardImage={4} vertical={true} />
+        </section>
       </section>
     </>
   );
