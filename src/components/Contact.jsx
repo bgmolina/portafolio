fd6bef88c4db4fe701b58f07e8f3880f4a9fb94c
Braian Molina <braian_gabriel_1993@hotmail.com>
2024-04-14 00:21:10 -0300

    se mueve id de ancla a img en Contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,34 +10,27 @@ export const Contact = () => {
       <section className="flex flex-row justify-center gap-x-5">
         <figure className="contactIcon">
           <a
-            id="idLinkedin"
             href="https://www.linkedin.com/in/braian-gabriel-molina/"
             target="_blank"
             rel="linkedin"
           >
-            <img src={linkedin} alt="linkedin" />
+            <img id="idLinkedin" src={linkedin} alt="linkedin" />
+          </a>
+        </figure>
+
+        <figure className="contactIcon">
+          <a href="https://github.com/bmolina1993" target="_blank" rel="github">
+            <img id="idGitHub" src={gitHub} alt="gitHub" />
           </a>
         </figure>
 
         <figure className="contactIcon">
           <a
-            id="idGitHub"
-            href="https://github.com/bmolina1993"
-            target="_blank"
-            rel="github"
-          >
-            <img src={gitHub} alt="gitHub" />
-          </a>
-        </figure>
-
-        <figure className="contactIcon">
-          <a
-            id="idOutlook"
             href="mailto:braian_gabriel_1993@hotmail.com?Subject=[Portafolio] - Contacto"
             target="_blank"
             rel="correo"
           >
-            <img src={outlook} alt="outlook" />
+            <img id="idOutlook" src={outlook} alt="outlook" />
           </a>
         </figure>
       </section>
